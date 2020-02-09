facba00f1d2b61c649750d6a9f51e13b4a5b9b5c
Rabih Malaeb <rabmalaeb@gmail.com>
2020-02-09 09:50:09 +0200

    refactor translations

--- src/app/modules/translation/translation-add/translation-add.component.ts
+++ src/app/modules/translation/translation-add/translation-add.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  Validators,
-  FormGroup,
-  FormBuilder,
-  FormGroupDirective
-} from '@angular/forms';
-import { ValidationMessagesService } from 'src/app/services/validation-messages.service';
 import { NotificationService } from 'src/app/services/notification.service';
-import { ActionType, ALERT_MESSAGES, ModuleName } from 'src/app/models/general';
+import { ActionType, ModuleName } from 'src/app/models/general';
 import { ActivatedRoute } from '@angular/router';
 import { Translation } from '../translation';
 import { Partner } from '../../partner/partner';
@@ -20,10 +13,10 @@ import {
   PartnerStoreActions,
   PartnerStoreSelectors
 } from 'src/app/root-store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { TranslationStoreSelectors, TranslationStoreActions } from '../store';
 import { ActionTypes } from '../store/actions';
-import { filter, map } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import {
   LanguageStoreActions,
   LanguageStoreSelectors
@@ -41,9 +34,6 @@ import { ErrorHandlerService } from 'src/app/services/error-handler.service';
 })
 export class TranslationAddComponent implements OnInit {
   constructor(
-    private form: FormBuilder,
-
-    private validationMessagesService: ValidationMessagesService,
     private authorizationService: AuthorizationService,
     private notificationService: NotificationService,
     private errorHandler: ErrorHandlerService,
@@ -52,14 +42,12 @@ export class TranslationAddComponent implements OnInit {
     private store$: Store<RootStoreState.State>
   ) {}
 
-  translationForm: FormGroup;
   actionType: ActionType;
   translation: Translation;
-  isLoadingTranslation = false;
-  isLoadingLanguages = false;
-  isLoadingPartners = false;
-  isLoadingLanguageKeys = false;
-  isLoading = false;
+  isLoadingTranslation$: Observable<boolean>;
+  isLoadingLanguages$: Observable<boolean>;
+  isLoadingPartners$: Observable<boolean>;
+  isLoadingLanguageKeys$: Observable<boolean>;
   partners$: Observable<Partner[]>;
   languages$: Observable<Language[]>;
   languageKeys$: Observable<LanguageKey[]>;
@@ -81,7 +69,6 @@ export class TranslationAddComponent implements OnInit {
         this.actionType = ActionType.EDIT;
       } else {
         this.actionType = ActionType.ADD;
-        this.buildNewTranslationForm();
       }
     });
   }
@@ -93,6 +80,10 @@ export class TranslationAddComponent implements OnInit {
 
     this.isLoadingAction$ = this.store$.select(
       TranslationStoreSelectors.selectIsLoadingAction
+    );
+
+    this.isLoading$ = this.store$.select(
+      TranslationStoreSelectors.selectIsLoadingItem
     );
 
     this.actionsSubject$
@@ -131,37 +122,21 @@ export class TranslationAddComponent implements OnInit {
     this.translation$ = this.store$.select(
       TranslationStoreSelectors.selectTranslationById(id)
     );
+    this.isLoading$ = this.store$.select(
+      TranslationStoreSelectors.selectIsLoadingItem
+    );
     this.loadingErrors$ = this.store$.select(
       TranslationStoreSelectors.selectTranslationLoadingError
     );
-    this.buildExistingTranslationForm();
-  }
-
-  buildNewTranslationForm() {
-    this.translationForm = this.form.group({
-      languageId: ['', [Validators.required]],
-      partnerId: ['', [Validators.required]],
-      languageKeyId: ['', [Validators.required]],
-      value: ['', [Validators.required]]
-    });
-  }
-
-  buildExistingTranslationForm() {
-    this.translation$.subscribe(translation => {
-      this.translation = translation;
-      this.translationForm = this.form.group({
-        languageId: [translation.languageId, [Validators.required]],
-        partnerId: [translation.partnerId, [Validators.required]],
-        languageKeyId: [translation.languageKeyId, [Validators.required]],
-        value: [translation.value, [Validators.required]]
-      });
-    });
   }
 
   getLanguages() {
     this.store$.dispatch(new LanguageStoreActions.LoadRequestAction());
     this.languages$ = this.store$.select(
       LanguageStoreSelectors.selectAllLanguageItems
+    );
+    this.isLoadingLanguages$ = this.store$.select(
+      LanguageStoreSelectors.selectIsLoadingItem
     );
   }
 
@@ -170,6 +145,9 @@ export class TranslationAddComponent implements OnInit {
     this.partners$ = this.store$.select(
       PartnerStoreSelectors.selectAllPartnerItems
     );
+    this.isLoadingPartners$ = this.store$.select(
+      PartnerStoreSelectors.selectIsLoadingItem
+    );
   }
 
   getLanguageKeys() {
@@ -177,69 +155,29 @@ export class TranslationAddComponent implements OnInit {
     this.languageKeys$ = this.store$.select(
       LanguagekeyStoreSelectors.selectAllLanguagekeyItems
     );
+    this.isLoadingLanguageKeys$ = this.store$.select(
+      LanguagekeyStoreSelectors.selectIsLoadingItem
+    );
   }
 
-  get languageId() {
-    return this.translationForm.get('languageId');
-  }
-
-  get partnerId() {
-    return this.translationForm.get('partnerId');
-  }
-
-  get languageKeyId() {
-    return this.translationForm.get('languageKeyId');
-  }
-
-  get value() {
-    return this.translationForm.get('value');
-  }
-
-  performAction(formData: any, formDirective: FormGroupDirective) {
-    if (!this.translationForm.valid) {
-      this.notificationService.showError(ALERT_MESSAGES.FORM_NOT_VALID);
-      return;
-    }
-    if (this.translation) {
-      this.updateTranslation(this.buildTranslationParams());
+  performAction(translation: Translation) {
+    if (this.actionType === ActionType.EDIT) {
+      this.updateTranslation(translation);
     } else {
-      this.addTranslation(this.buildTranslationParams());
+      this.addTranslation(translation);
     }
   }
 
-  buildTranslationParams(): Translation {
-    const translation = new Translation();
-    translation.partnerId = this.partnerId.value;
-    translation.languageId = this.languageId.value;
-    translation.languageKeyId = this.languageKeyId.value;
-    translation.value = this.value.value;
-    return translation;
-  }
-
-  addTranslation(params: Translation) {
+  addTranslation(translation: Translation) {
     this.store$.dispatch(
-      new TranslationStoreActions.AddTranslationRequestAction(params)
+      new TranslationStoreActions.AddTranslationRequestAction(translation)
     );
   }
 
-  updateTranslation(params: Translation) {
-    const id = this.translation.id;
+  updateTranslation(translation: Translation) {
+    const id = translation.id;
     this.store$.dispatch(
-      new TranslationStoreActions.UpdateTranslationRequestAction(id, params)
-    );
-  }
-
-  get buttonLabel() {
-    return this.isLoadingAction$.pipe(
-      map(isLoading => {
-        if (isLoading) {
-          return 'Loading';
-        }
-        if (this.actionType === ActionType.EDIT) {
-          return 'Update';
-        }
-        return 'Add';
-      })
+      new TranslationStoreActions.UpdateTranslationRequestAction(id, translation)
     );
   }
 
@@ -250,13 +188,9 @@ export class TranslationAddComponent implements OnInit {
     return 'Add Translation';
   }
 
-  get validationMessages() {
-    return this.validationMessagesService.getValidationMessages();
-  }
-
-  get canEditTranslation() {
+  get canEditTranslation$() {
     if (this.actionType === ActionType.ADD) {
-      return true;
+      return of(true);
     }
     return (
       this.actionType === ActionType.EDIT &&
--- src/app/modules/translation/translation-form/translation-form.component.ts
+++ src/app/modules/translation/translation-form/translation-form.component.ts
@@ -1,15 +1,137 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges
+} from '@angular/core';
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormGroupDirective
+} from '@angular/forms';
+import { ValidationMessagesService } from 'src/app/services/validation-messages.service';
+import { NotificationService } from 'src/app/services/notification.service';
+import { ActionType, ALERT_MESSAGES } from 'src/app/models/general';
+import { Translation } from '../translation';
+import { Language } from '../../language/language';
+import { Partner } from '../../partner/partner';
+import { LanguageKey } from '../../language-key/language-key';
 
 @Component({
   selector: 'app-translation-form',
   templateUrl: './translation-form.component.html',
   styleUrls: ['./translation-form.component.sass']
 })
-export class TranslationFormComponent implements OnInit {
+export class TranslationFormComponent implements OnInit, OnChanges {
+  constructor(
+    private form: FormBuilder,
+    private notificationService: NotificationService,
+    private validationMessagesService: ValidationMessagesService
+  ) { }
 
-  constructor() { }
+  translationForm: FormGroup;
+  @Input() translation: Translation;
+  @Input() actionType: ActionType;
+  @Input() languages: Language[];
+  @Input() partners: Partner[];
+  @Input() languageKeys: LanguageKey[];
+  @Input() isLoadingAction: boolean;
+  @Input() canEditTranslation = false;
+  @Input() isLoading: boolean;
+  @Input() isLoadingLanguageKeys: boolean;
+  @Input() isLoadingPartners: boolean;
+  @Input() isLoadingLanguages: boolean;
+  @Output() submitForm = new EventEmitter<Translation>();
+  formGroupDirective: FormGroupDirective;
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngOnChanges() {
+    if (this.isLoadingAction) {
+      return false;
+    }
+    if (this.translation) {
+      this.buildExistingTranslationForm();
+    } else {
+      this.buildNewTranslationForm();
+      if (this.formGroupDirective) {
+        this.formGroupDirective.resetForm();
+      }
+    }
   }
 
+  buildNewTranslationForm() {
+    this.translationForm = this.form.group({
+      languageId: ['', [Validators.required]],
+      partnerId: ['', [Validators.required]],
+      languageKeyId: ['', [Validators.required]],
+      value: ['', [Validators.required]]
+    });
+  }
+
+  buildExistingTranslationForm() {
+    this.translationForm = this.form.group({
+      id: [this.translation.id],
+      languageId: [this.translation.languageId, [Validators.required]],
+      partnerId: [this.translation.partnerId, [Validators.required]],
+      languageKeyId: [this.translation.languageKeyId, [Validators.required]],
+      value: [this.translation.value, [Validators.required]]
+    });
+  }
+
+  get languageId() {
+    return this.translationForm.get('languageId');
+  }
+
+  get partnerId() {
+    return this.translationForm.get('partnerId');
+  }
+
+  get languageKeyId() {
+    return this.translationForm.get('languageKeyId');
+  }
+
+  get value() {
+    return this.translationForm.get('value');
+  }
+
+  buildTranslationParams(): Translation {
+    const translation = new Translation();
+    translation.id = this.translationForm.get('id') ? this.translationForm.get('id').value : '',
+    translation.partnerId = this.partnerId.value;
+    translation.languageId = this.languageId.value;
+    translation.languageKeyId = this.languageKeyId.value;
+    translation.value = this.value.value;
+    return translation;
+  }
+
+  get buttonLabel() {
+    if (this.isLoadingAction) {
+      return 'Loading';
+    }
+    if (this.actionType === ActionType.EDIT) {
+      return 'Update';
+    }
+    return 'Add';
+  }
+
+  performAction(formDirective: FormGroupDirective) {
+    this.formGroupDirective = formDirective;
+    if (!this.translationForm.valid) {
+      this.notificationService.showError(ALERT_MESSAGES.FORM_NOT_VALID);
+      return;
+    }
+    this.submitForm.emit(this.buildTranslationParams());
+  }
+
+  get validationMessages() {
+    return this.validationMessagesService.getValidationMessages();
+  }
+
+  get isLoadingForm() {
+    return !this.translation && this.isLoading;
+  }
 }
