dd003dfdb10811a56baf1af74b5d603bc5381cbb
Rabih Malaeb <rabmalaeb@gmail.com>
2020-07-11 08:12:46 +0300

    updat

--- src/app/shared/components/nav-item/nav-item.component.ts
+++ src/app/shared/components/nav-item/nav-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NavItem, NavItemOptions } from '../../models/nav';
+import { NavItemOptions } from '../../models/nav';
 
 @Component({
   selector: 'app-nav-item',
